cc4cb6787e3b783016292740c5a335f9105f441f
Aj-Ziad <ajdourziyad@gmail.com>
2026-01-11 23:55:12 +0100

    add stuff

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,11 +22,11 @@ function Contact() {
             </div>
             <div>
               <h3 className="text-xl font-semibold text-gray-900 mb-2">Phone</h3>
-              <p className="text-lg text-gray-600">+1 (555) 123-4567</p>
+              <p className="text-lg text-gray-600">+212 (555) 123-4567</p>
             </div>
             <div>
               <h3 className="text-xl font-semibold text-gray-900 mb-2">Address</h3>
-              <p className="text-lg text-gray-600">123 Digital Street, Tech City, TC 12345</p>
+              <p className="text-lg text-gray-600">Agadir ,Maroc</p>
             </div>
           </div>
         </div>
